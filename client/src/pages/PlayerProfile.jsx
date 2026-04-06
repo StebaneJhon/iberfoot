53e60f00d2ff3bb393479b0076dbae765c5a9040
StebaneJhon <111849849+StebaneJhon@users.noreply.github.com>
2026-04-06 14:36:38 +0200

    player profile
    responsive

--- client/src/pages/PlayerProfile.jsx
+++ client/src/pages/PlayerProfile.jsx
@@ -2,14 +2,16 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import "../../public/player-profile.css";
+import Footer from "../components/Footer";
 
 function PlayerProfile() {
 
     const { id } = useParams();
-    const [player, setPlayer] = useState(null);
+    const [player, setPlayer] = useState();
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
+        window.scrollTo(0, 0);
         fetch(`http://localhost:3000/api/players/${id}`)
             .then(res => res.json())
             .then(data => {
@@ -22,49 +24,50 @@ function PlayerProfile() {
     if (loading) return <p>Loading player profile...</p>;
     if (!player) return <p>Player not found.</p>;
 
-    const {name, position, team, image, age, transfermarkt, dynamic_photos} = player;
+    const {name, position, team, image, age, transfermarkt, dynamic_photos} = player
 
     return (
-        <div className="profile-page-bg">
+        <div>
             <div className="player-profile-container">
-                <aside className="profile-sidebar">
-                    <div className="profile-image-container">
-                        {image ? (
-                            <img 
-                                className="image"
-                                src={image} 
-                                alt={name} 
-                            />
-                        ) : (
-                            <div>
-                                No Image
-                            </div>
-                        )}
-                    </div>
-                    <h3 className="player-profile-name" >{name}</h3>
-                    <p className='player-profile-age'><span>Age:</span> {age}</p>
-                    <p className="player-profile-position" ><span>Position:</span> {position}</p>
-                    <p className="player-profile-team" ><span>Team:</span> {team}</p>
-                    <div className="social-btn-container">
-                        <Link to={transfermarkt} className='social-btn'>TM</Link>
-                    </div>
-                </aside>
+                <div className="profile-sidebar">
+                <div className="profile-image-container">
+                    {image ? (
+                        <img 
+                            className="image"
+                            src={image} 
+                            alt={name} 
+                        />
+                    ) : (
+                        <div>
+                            No Image
+                        </div>
+                    )}
+                </div>
+                <h3 className="player-profile-name" >{name}</h3>
+                <p className='player-profile-age'><span>Age:</span> {age}</p>
+                <p className="player-profile-position" ><span>Position:</span> {position}</p>
+                <p className="player-profile-team" ><span>Team:</span> {team}</p>
+                <div className="social-btn-container">
+                    <Link to={transfermarkt} className='social-btn'>TM</Link>
+                </div>
+            </div>
 
-                <main>
-                        {(!dynamic_photos || dynamic_photos.length === 0) ? (
-                            <div className='profile-gallery-empty'>
-                                <p>No action shots available for this player.</p>
-                            </div>
-                        ) : (
-                            <div className="profile-gallery"> 
-                                {dynamic_photos.map((d, index) => (
-                                    <div className='gallery-item' key={index}> 
-                                        <img className="image" src={d.url} alt='Dynamic photo'/>
-                                    </div>
-                                ))}
-                            </div>
-                        )}
-                </main>
+            {(!dynamic_photos || dynamic_photos.length === 0) ? (
+                <div className='profile-gallery-empty'>
+                    <p>No action shots available for this player.</p>
+                </div>
+            ) : (
+                <div className="profile-gallery"> 
+                    {dynamic_photos.map((d, index) => (
+                        <div className='gallery-item' key={d.id || index}> 
+                            <img className="image" src={d.url} alt='Dynamic photo'/>
+                        </div>
+                    ))}
+                </div>
+            )}
+            </div>
+            <div className="footer-section player-profile-footer-section">
+                <Footer />
             </div>
         </div>
     )
